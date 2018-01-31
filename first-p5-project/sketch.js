8cc0e797e15ec8b94d442ce6542ecdb1c639ef6a
Magapiggy <35992241+Magapiggy@users.noreply.github.com>
2018-01-31 01:57:25 -0800

    Drawing shapes, changing background colour

--- first-p5-project/sketch.js
+++ first-p5-project/sketch.js
@@ -1,7 +1,13 @@
-function setup(){
-
+   function setup() {
+    createCanvas(800 600);
+    background(131,200,212);
 }
 
 function draw(){
-
+ rect(100,150,100,50)
+rect(100,150,100,50)
+rect(100,150,100,6)
+rect(400,150,50,50)
+rect(400,150,100,50)
+rect(400,150,100,6)
 }
